041effdb350fff58a2d7ae72ad99f15fa28d39f7
Nick Howard <nick.howard.81@gmail.com>
2025-03-13 17:48:57 +1100

    Convert to striongs

--- ninja2mqtt.py
+++ ninja2mqtt.py
@@ -124,15 +124,15 @@ def process_ninjacape_messages():
                     dev_id = str(device["D"])
                     dev_value = str(device["DA"])
 
-                    logging.info(f"prior")
-                    if dev_id in {"999", "1007"}:
+                    #logging.info(f"prior")
+                    if str(dev_id) in {"999", "1007"}:
                         dev_moderated_value = hex_to_tuple(dev_value)
                     else:
                         dev_moderated_value = dev_value
-                    logging.info(f"later")
-                    
+                    #logging.info(f"later")
+
                     # Publish received sensor data to MQTT
-                    mqtt_client.publish(f"ninjaCape/input/{dev_id}", dev_moderated_value, qos=0, retain=True)
+                    mqtt_client.publish(f"ninjaCape/input/{dev_id}", str(dev_moderated_value), qos=0, retain=True)
                     logging.info(f"Published sensor {dev_id} -> {dev_moderated_value}")
                 else:
                     logging.warning(f"Unknown data format received: {line}")
