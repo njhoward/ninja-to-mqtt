1ed846da5aa2dc1f9e14feb6f3b51ffd12e6e037
Nick Howard <nick.howard.81@gmail.com>
2025-04-17 14:28:10 +1000

    unit tests suspicious log

--- test_ninja2mqtt.py
+++ test_ninja2mqtt.py
@@ -1,8 +1,14 @@
+import pytest
 import config
 import utils
 import logger
 import notifier
 import serialhandler
+import logging.config
+import json
+from rfhandler import log_if_suspicious_rf
+
+logging.config.fileConfig("logging.conf")
 
 def test_config_loaded():
     assert hasattr(config, 'MQTT_BROKER')
@@ -18,3 +24,11 @@ def test_notifier_mock():
 
 def test_serialhandler_mock():
     assert hasattr(serialhandler, 'init_serial')
+
+@pytest.mark.parametrize("payload", [
+    {"DEVICE": [{"G": "0", "V": 2, "D": 11, "DA": 123456}]},
+    {"DEVICE": [{"G": "0", "V": 5, "D": 11, "DA": str(0x30000000)}]},  # house = 3
+    {"DEVICE": [{"G": "0", "V": 5, "D": 11, "DA": "garbage"}]},
+])
+def test_suspicious_detection(payload):
+    log_if_suspicious_rf(payload, raw_line=json.dumps(payload))
