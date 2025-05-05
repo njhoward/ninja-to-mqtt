122a66c01b63e988a6c7803568a7b5b426390003
Nick Howard <nick.howard.81@gmail.com>
2025-05-05 10:26:39 +1000

    fix blink debug

--- mqttdebugs.py
+++ mqttdebugs.py
@@ -2,7 +2,7 @@
 import logging
 import time
 from config import STATUS_LED_ID, EYES_LED_ID
-from scheduler import perform_hourly_blink, choose_blink_color, send_led
+from scheduler import perform_blink, choose_blink_color, send_led
 from statehandler import get_all_states, current_states
 
 def handle_debugs(mqttclient, topic, payload):
@@ -24,13 +24,13 @@ def handle_debugs(mqttclient, topic, payload):
                 time.sleep(1)
                 send_led(1007, "00FF00")
             else:
-                hour = int(payload) if payload.isdigit() else 12
+                count = int(payload) if payload.isdigit() else 12
                 status_before = current_states.get(str(STATUS_LED_ID), "0000FF")
                 eyes_before = current_states.get(str(EYES_LED_ID), "0000FF")
                 blink_color = choose_blink_color(status_before, eyes_before)
 
-                perform_hourly_blink(hour, blink_color, status_before, eyes_before)
-                logging.info(f"Manually triggered hourly blink for {hour} o'clock")
+                perform_blink(count, blink_color, status_before, eyes_before)
+                logging.info(f"Manually triggered blink for {hour} o'clock")
         except Exception as e:
             logging.error(f"Failed to trigger manual blink: {e}")
-        return f"[Debug] Blink triggered for {hour}"  # or return True
+        return f"[Debug] Blink triggered for {count} blinks"  # or return True
